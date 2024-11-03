fea015822aa7302845f5f405a23b80794a86a0ef
sooda <shin.et0613@gmail.com>
2024-11-03 13:31:01 +0900

    s

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const canvas =document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d"); 
 
 const image = new Image();
-image.src = "sprit.jpeg";
+image.src = 'sprit.jpeg';
 
 image.onload = () =>{
     ctx.drawImage(image,0,0);
