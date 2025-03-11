cef4c732dafec9262ab85458d6d97cf1637fe57a
sooda <shin.et0613@gmail.com>
2025-03-11 14:59:34 +0900

    a

--- main.js
+++
@@ -1,9 +0,0 @@
-const canvas =document.getElementById("myCanvas");
-const con = canvas.getContext("2d"); 
-
-const image = new Image();
-image.src = 'sprit.jpeg';
-
-image.onload = () =>{
-    con.drawImage(image,0,0,150,200);
-};
