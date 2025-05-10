0ca1c540a9503bd1856818d327053a8c009a2702
sooda <shin.et0613@gmail.com>
2025-05-10 10:15:02 +0900

    v

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ const block = {
 //押したとき
 document.addEventListener('keydown',(e)=>{
     keys[e.code]=true;
+
+
 });
 
 //引いたとき
@@ -34,7 +36,13 @@ function drawplyer(){
     ctx.fillRect(plyer.x, plyer.y, plyer.width, plyer.height);
 }
 
-//
+//剣の描画
+function drawsood(){
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(sood.x, sood.y, sood.width, sood.height);
+}
+
+//敵の描画
 function drawEnemies(){
     ctx.fillStyle = 'red';
     ctx.fillRect(enemies.x, enemies.y, enemies.width, enemies.height);
